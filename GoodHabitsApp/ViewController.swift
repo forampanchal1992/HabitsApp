4543e77f69d7d823a85dac13998a356cc8f91203
forampanchal <foram.panchal92@yahoo.com>
2019-04-07 16:02:21 -0400

    Initial commit

---
+++ GoodHabitsApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GoodHabitsApp
+//
+//  Created by Foram Panchal on 2019-04-07.
+//  Copyright © 2019 Foram Panchal. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
