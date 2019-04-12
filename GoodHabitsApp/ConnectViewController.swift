716c4b868799701026e5e44d992f9c7189b7a27e
himauli_patel <himaulipatel2195@gmail.com>
2019-04-12 09:44:23 -0400

    Validation added

--- GoodHabitsApp/ConnectViewController.swift
+++ GoodHabitsApp/ConnectViewController.swift
@@ -22,7 +22,7 @@ class ConnectViewController: UIViewController {
     
     var name : String = ""
     var code : String = ""
-    let accessCode = arc4random()
+    var friendName : String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,20 +31,17 @@ class ConnectViewController: UIViewController {
         
         let sharedPreferences = UserDefaults.standard
         self.name = sharedPreferences.string(forKey: "Name")!
-        self.code = sharedPreferences.string(forKey: "Access Code")!
         
         if (name == nil) {
             name = "Name"
-            print("Enter your name")
+            print("No name found")
         }
         else {
             print("Name: \(name) And Code: \(code)")
             nameLabel.text = "Hello \(name)"
-            accessCodeLabel.text = "Access Code : \(code)"
         }
     }
     
-
     /*
     // MARK: - Navigation
 
@@ -55,71 +52,47 @@ class ConnectViewController: UIViewController {
     }
     */
     
-    @IBAction func askFriend(_ sender: Any) {
-        
-        let alert = UIAlertController(title: "Wait for friend to join", message: "generateCode()", preferredStyle: .alert)
-         self.db.child("Friends").child("Access Code").child(String(accessCode)).child("Name1").setValue(name)
-        
-        handle = db?.child("Friends").child("Access Code").child(String(accessCode)).child("areFriendsConnected").observe(.value, with: { (snapshot) in
-            
-//            print("Snapshot: \(snapshot)")
-            
-            let checker:Bool!
-            checker = snapshot.value as! Bool
-            
-            print("Checker: \(checker!)")
-            
-            if (checker == true)
-            {
-                // navigate to next page
-            }
-        })
-        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: nil))
-//        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
-        
-        self.present(alert, animated: false)
-    }
-    
-
     @IBAction func helpFriendClicked(_ sender: Any) {
       
-        var inputCode : String = ""
-        
         let alert = UIAlertController(title: "Join a Friend", message: "Enter Friend's name and code",preferredStyle: .alert)
         
-        alert.addTextField { (textField) in
-            textField.text = ""
-            textField.placeholder = "Enter name here"
+        alert.addTextField { (friendToConnect) in
+            friendToConnect.text = ""
+            friendToConnect.placeholder = "Enter name here"
         }
         
         alert.addAction(UIAlertAction(title: "Join", style: .default, handler: { [weak alert] (_) in
-            let textField = alert?.textFields![0] // Force unwrapping because we know it exists.
-            print("Text field: \(textField!.text)")
-            inputCode = (textField?.text)!
+            let friendToConnect = alert?.textFields![0] // Force unwrapping because we know it exists.
+            print("Text field: \(friendToConnect!.text)")
+            self.friendName = (friendToConnect?.text)!
             
-            self.db?.child("Friends").child(String(inputCode)).observe(.value, with: { (snapshot) in
+            self.db?.child("Friends").child(String(self.friendName)).observe(.value, with: { (snapshot) in
                 
                 print("=============\(snapshot)")
                 
-                let friends = snapshot.value as? NSDictionary
-                let newFriend = friends!["Name"] as? String
-                
-                if (newFriend == inputCode)
+                if(snapshot.exists())
                 {
-                    
-                    self.db.child("Friends").child(self.name).child("areFriendsConnected").setValue(true)
-                    self.db.child("Friends").child(self.name).child("Connected with").setValue(inputCode)
-                    self.db.child("Friends").child(String(inputCode)).child("areFriendsConnected").setValue(true)
-                    self.db.child("Friends").child(String(inputCode)).child("Connected with").setValue(self.name)
+                    print("\(self.friendName) found")
+                    let friends = snapshot.value as? NSDictionary
+                    let newFriend = friends!["Name"] as? String
+    
+                    if (newFriend == self.friendName)
+                    {
+                        self.db.child("Friends").child(self.name).child("areFriendsConnected").setValue(true)
+                        self.db.child("Friends").child(self.name).child("Connected with").setValue(self.friendName)
+                        self.db.child("Friends").child(String(self.friendName)).child("areFriendsConnected").setValue(true)
+                        self.db.child("Friends").child(String(self.friendName)).child("Connected with").setValue(self.name)
+                    }
+                }
+                else
+                {
+                    print("\(self.friendName) is not found")
+                    let friendNotFoundAlert = UIAlertController(title: "No person found", message: "No person with id \(self.friendName)",preferredStyle: .alert)
+                    friendNotFoundAlert.addAction(UIAlertAction(title: "Try again", style: .default, handler: nil))
                 }
                 
             })
-            
         }))
-        
-        
         self.present(alert, animated: true)
-       
     }
-    
 }
--- GoodHabitsApp/ViewController.swift
+++ GoodHabitsApp/ViewController.swift
@@ -18,30 +18,20 @@ class ViewController: UIViewController {
     
     //Variables
     var inputNameText : String = ""
-    let accessCode = arc4random()
    
     var db:DatabaseReference!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         self.db = Database.database().reference()
-        
-        
     }
     
     @IBAction func NextButtonClicked(_ sender: Any) {
         self.inputNameText = inputName.text!
-        
-        print("Access Code : ",accessCode)
-        
-        let data = ["Access Code" : accessCode,"Name" : inputNameText,"areFriendsConnected" : false] as [String : Any]
-//        self.db.child("Friends").child("Access Code").child(String(accessCode)).setValue(data)
+        let data = ["Name" : inputNameText,"areFriendsConnected" : false] as [String : Any]
         self.db.child("Friends").child(String(inputNameText)).setValue(data)
         let sharedPreferences = UserDefaults.standard
         sharedPreferences.set(self.inputNameText, forKey:"Name")
-        sharedPreferences.set(self.accessCode, forKey: "Access Code")
-        print("Saved \(self.inputNameText) and \(self.accessCode) to shared preferences!")
-        
+        print("Saved \(self.inputNameText) to shared preferences!")
     }
 
     
