9d753f694ed856a71874a8a49222ff08d58cd3f4
himauli_patel <himaulipatel2195@gmail.com>
2019-04-14 22:09:34 -0400

    Habit added to a person's list

--- GoodHabitsApp/ConnectViewController.swift
+++ GoodHabitsApp/ConnectViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 import FirebaseDatabase
 import Firebase
 
-class ConnectViewController: UIViewController{
+class ConnectViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate{
     
     @IBOutlet weak var nameLabel: UILabel!
     
-    @IBOutlet weak var dropdown: UIPickerView!
+    @IBOutlet weak var customeHabitTextField: UITextField!
     @IBOutlet weak var toTextField: UITextField!
+    
+    var currentTextField = UITextField()
+    var pickerView = UIPickerView()
+   
     @IBOutlet weak var habitLabelTwo: UILabel!
     @IBOutlet weak var habitLabelOne: UILabel!
     
@@ -45,58 +49,13 @@ class ConnectViewController: UIViewController{
         }
         else {
             print("Name: \(name) And Code: \(code)")
-            nameLabel.text = "Hello \(name)"
+            nameLabel.text = "\(name)"
         }
-        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        toTextField.inputView = pickerView
         getHabits()
     }
-    
-//    @IBAction func helpFriendClicked(_ sender: Any) {
-//
-//        let alert = UIAlertController(title: "Join a Friend", message: "Enter Friend's name and code",preferredStyle: .alert)
-//
-//        alert.addTextField { (friendToConnect) in
-//            friendToConnect.text = ""
-//            friendToConnect.placeholder = "Enter name here"
-//        }
-//
-//        alert.addAction(UIAlertAction(title: "Join", style: .default, handler: { [weak alert] (_) in
-//            let friendToConnect = alert?.textFields![0] // Force unwrapping because we know it exists.
-//            print("Text field: \(friendToConnect!.text)")
-//            self.friendName = (friendToConnect?.text)!
-//
-//            self.db?.child("Friends").child(String(self.friendName)).observe(.value, with: { (snapshot) in
-//
-//                print("=============\(snapshot)")
-//
-//                if(snapshot.exists())
-//                {
-//                    print("\(self.friendName) found")
-//                    //                    let friends = snapshot.value as? NSDictionary
-//                    //                    let newFriend = friends!["Name"] as? String
-//
-//                    self.db.child("Friends").child(self.name).child("areFriendsConnected").setValue(true)
-//                    self.db.child("Friends").child(self.name).child("Connected with").setValue(self.friendName)
-//
-//                self.db.child("Friends").child(String(self.friendName)).child("areFriendsConnected").setValue(true)
-//                    self.db.child("Friends").child(String(self.friendName)).child("Connected with").setValue(self.name)
-//
-//
-//                self.db.child("Friends").child(String(self.friendName)).child("areFriendsConnected").setValue(true)
-//                    self.db.child("Friends").child(String(self.friendName)).child("Connected with").setValue(self.name)
-//
-//
-//                }
-//                else
-//                {
-//                    print("\(self.friendName) is not found")
-//                    //                    let friendNotFoundAlert = UIAlertController(title: "No person found", message: "No person with id \(self.friendName)",preferredStyle: .alert)
-//                    //                    friendNotFoundAlert.addAction(UIAlertAction(title: "Try again", style: .default, handler: nil))
-//                }
-//            })
-//        }))
-//        self.present(alert, animated: true)
-//    }
     
     func getHabits(){
         self.db?.child("Habits").observe(.value, with: { (snapshot) in
@@ -113,8 +72,56 @@ class ConnectViewController: UIViewController{
                     print("Value of key : \(valueOfKey)")
                     self.habits.append(valueOfKey)
                 }
-                print("Habis from dictionary : \(self.habits)")
+                print("Habis from Array of String : \(self.habits)")
             }
         })
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return habits.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+            toTextField.text = habits[row]
+            self.view.endEditing(true)
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            return habits[row]
+        
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("+++++Begin editing++++++")
+        currentTextField = textField
+        toTextField.inputView = pickerView
+        
+    }
+    
+    @IBAction func startButtonClicked(_ sender: Any) {
+        
+        var selectedHabit = toTextField.text
+        var createdHabit = customeHabitTextField.text
+        
+        print("Selected: \(selectedHabit!)")
+        print("Created: \(createdHabit!)")
+        
+        if (selectedHabit != nil || createdHabit != nil)
+        {
+            if(selectedHabit != nil)
+            {
+                print("Selected habit is: \(selectedHabit!)")
+                self.db.child("Friends").child(String(name)).child("Habit").setValue(selectedHabit!)
+            }
+            else if(createdHabit != nil)
+            {
+                print("Created habit is: \(createdHabit!)")
+                self.db.child("Friends").child(String(name)).child("Habit").setValue(createdHabit!)
+            }
+        }
+    }
+    
 }
---
+++ GoodHabitsApp/FriendViewController.swift
@@ -0,0 +1,90 @@
+//
+//  FriendViewController.swift
+//  GoodHabitsApp
+//
+//  Created by Himauli Patel on 2019-04-14.
+//  Copyright © 2019 Foram Panchal. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+import Firebase
+
+class FriendViewController: UIViewController {
+
+    var db:DatabaseReference!
+    var name : String = ""
+    var friendName : String = ""
+    @IBOutlet weak var friendNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.db = Database.database().reference()
+        
+        let sharedPreferences = UserDefaults.standard
+        self.name = sharedPreferences.string(forKey: "Name")!
+        
+        self.db?.child("Friends").child(String(name)).observe(.value, with:
+        {(snapshot) in
+//            print("ConnectedWith : \(snapshot)")
+            
+            if(snapshot.hasChild("connectedWith"))
+            {
+                print("Connected with \(snapshot)")
+                let friend = snapshot.value(forKey: "connectedWith")
+                print("Friend is --> \(friend as! String)")
+            }
+            else
+            {
+                print("Connecting........")
+                self.connectFriend()
+//                let friend = snapshot.value(forKey: "connectedWith")
+//                print("Friend is --> \(friend as! String)")
+            }
+        })
+    }
+    
+    func connectFriend() {
+        let alert = UIAlertController(title: "Join a Friend", message: "Enter Friend's name",preferredStyle: .alert)
+        
+        alert.addTextField { (friendToConnect) in
+            friendToConnect.text = ""
+            friendToConnect.placeholder = "Enter name here"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Join", style: .default, handler: { [weak alert] (_) in
+            let friendToConnect = alert?.textFields![0] // Force unwrapping because we know it exists.
+            print("Text field: \(friendToConnect!.text)")
+            self.friendName = (friendToConnect?.text)!
+            
+            self.db?.child("Friends").child(String(self.friendName)).observe(.value, with: { (snapshot) in
+                
+                print("=============\(snapshot)")
+                self.friendNameLabel.text = self.friendName
+                if(snapshot.exists())
+                {
+                    print("\(self.friendName) found")
+                    //                    let friends = snapshot.value as? NSDictionary
+                    //                    let newFriend = friends!["Name"] as? String
+                    
+                self.db.child("Friends").child(self.name).child("areFriendsConnected").setValue(true)
+                self.db.child("Friends").child(self.name).child("Connected with").setValue(self.friendName)
+                    
+            self.db.child("Friends").child(String(self.friendName)).child("areFriendsConnected").setValue(true)
+                self.db.child("Friends").child(String(self.friendName)).child("Connected with").setValue(self.name)
+                    
+                }
+                else
+                {
+                    print("\(self.friendName) is not found")
+                    //                    let friendNotFoundAlert = UIAlertController(title: "No person found", message: "No person with id \(self.friendName)",preferredStyle: .alert)
+                    //                    friendNotFoundAlert.addAction(UIAlertAction(title: "Try again", style: .default, handler: nil))
+                }
+            })
+        }))
+        self.present(alert, animated: true)
+    }
+
+
+}
